d1c6ef44079a9c5e1c334d131b5a6efd60c7f90f
Hamed Khomjani <64583620+hamedkhomjani@users.noreply.github.com>
2020-10-07 21:41:56 +0200

    saving dom objects to variables

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,3 +1,17 @@
 "use strict";
 
-$(function () {});
+$(function () {
+  var anim_id; //saving dom objects to variables
+
+  var container = $('.container');
+  var car_0 = $('#car_0');
+  var car_1 = $('#car_1');
+  var car_2 = $('#car_2');
+  var car_3 = $('#car_3');
+  var line_1 = $('#line_1');
+  var line_2 = $('#line_2');
+  var line_3 = $('#line_3');
+  var restart_btn = $('.restart_btn');
+  var restart = $('#restart');
+  var score = $('#score');
+});
